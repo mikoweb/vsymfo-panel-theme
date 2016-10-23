98188e72f06eb1490ae1afd7d4103f27ff12942d
Rafał Mikołajun <rafal@mikoweb.pl>
2016-10-23 18:35:58 +0200

    checkbox buttons style

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
-require(['jquery', 'select2', 'theme/bootstrap-layout'],
+require(['jquery', 'bootstrap-toggle', 'select2', 'theme/bootstrap-layout'],
 function ($) {
     "use strict";
+    $('input.input-toggle').bootstrapToggle();
     $('select.select2').select2();
 });
