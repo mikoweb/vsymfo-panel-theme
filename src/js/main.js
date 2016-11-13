7b12993c4bb37ef649a33d22436b5a94df1dfaea
Rafał Mikołajun <rafal@mikoweb.pl>
2016-11-13 13:02:00 +0100

    add datepicker

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,20 @@
-require(['jquery', 'theme__row-url-view', 'bootstrap-toggle', 'select2', 'theme__bootstrap-layout'],
-function ($, RowUrlView) {
+require(['jquery', 'theme__row-url-view', 'vsymfo/elements/date-picker', 'vsymfo/elements/datetime-picker',
+    'bootstrap-toggle', 'select2', 'theme__bootstrap-layout'],
+function ($, RowUrlView, DatePicker, DateTimePicker) {
     "use strict";
     var body = $('body');
+
     $('input.input-toggle').bootstrapToggle();
     $('select.select2').select2({
         width: '100%',
         allowClear: true
     });
+
     new RowUrlView({el: body});
+    new DatePicker.default({
+        el: $('input.input-datepicker')
+    });
+    new DateTimePicker.default({
+        el: $('input.input-datetimepicker')
+    });
 });
